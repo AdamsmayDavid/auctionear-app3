1c421670e4d62f520207b61907d57bbb6e60a23b
CubeDenmark <cubdesdenmark06242gmail.com>
2024-03-18 01:52:28 +0800

    jquery main on bidding

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -187,7 +187,7 @@
     </div>
 
       <!-- script file -->
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
  
            
 </body>
