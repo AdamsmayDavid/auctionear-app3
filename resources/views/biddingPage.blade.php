f4ffc3a7788aa2d3c17a8cbb2ad25c3e66c220ed
CubeDenmark <cubdesdenmark06242gmail.com>
2024-03-18 21:10:40 +0800

    ws links

--- resources/views/biddingPage.blade.php
+++ resources/views/biddingPage.blade.php
@@ -188,7 +188,7 @@
 </div> -->
 
 
-            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+            <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
 
 
 @if(!empty($auctionData))
