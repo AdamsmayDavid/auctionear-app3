2497011dee104beac84c4bdac7796b5128c1854b
CubeDenmark <cubdesdenmark06242gmail.com>
2024-03-18 13:30:32 +0800

    links added

--- resources/views/chats.blade.php
+++ resources/views/chats.blade.php
@@ -17,7 +17,9 @@
         <link rel="stylesheet" href="assets/css/chatStyling.css">
         <title>Chat</title>
     
- 
+    <!-- script file -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
     <!-- start: Chat -->
         
