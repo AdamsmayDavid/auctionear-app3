c88538f3f7e59b3e1d64c13b00f1e15a2b312344
CubeDenmark <cubesdenmark0624@gmail.com>
2024-03-23 01:53:30 +0800

    gfdgd

--- app/Http/Controllers/AuctionController.php
+++ app/Http/Controllers/AuctionController.php
@@ -205,21 +205,7 @@ class AuctionController extends Controller
          try {
 
             foreach ($bidderDetails as $aucBidder) {
-                if ($aucBidder['phone'] != $winnerPhone) {
-                    // Send loser message
-                    $response = $client->request('POST', 'https://api.httpsms.com/v1/messages/send', [
-                        'headers' => [
-                            'x-api-key' => $apiKey,
-                        ],
-                        'json' => [
-                            'content' => 'You lost the Auction',
-                            'from' => "+639916406021",
-                            'to' => '+63'.$aucBidder['phone']
-                        ]
-                    ]);
-                } else {
-                    
-
+                if ($aucBidder['phone'] == $winnerPhone) {
                     // Send winner message
                     $response = $client->request('POST', 'https://api.httpsms.com/v1/messages/send', [
                         'headers' => [
@@ -229,6 +215,18 @@ class AuctionController extends Controller
                             'content' => 'Congratulations! You won the Auction. Please go to Message to communicate with the auctioneer',
                             'from' => "+639916406021",
                             'to' => '+63'.$winnerPhone
+                        ]
+                    ]);
+                } else {
+                    // Send loser message
+                    $response = $client->request('POST', 'https://api.httpsms.com/v1/messages/send', [
+                        'headers' => [
+                            'x-api-key' => $apiKey,
+                        ],
+                        'json' => [
+                            'content' => 'You lost the Auction',
+                            'from' => "+639916406021",
+                            'to' => '+63'.$aucBidder['phone']
                         ]
                     ]);
                 }
